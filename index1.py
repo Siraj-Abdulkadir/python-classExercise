b9a95013299c43671246fa2536997acc625bf4b5
Siraj Abdulkadir <imsirajabdulkadir@gmail.com>
2024-12-13 13:20:15 +0300

    v2

--- index1.py
+++ index1.py
@@ -1,20 +1,15 @@
 
 playlist = []
 song=input("Please enter a song: " )
-addAnother = input("Do you want to add another song?: ")
+addAnother = input(f"Do you want to add another song ? (Enter no if not): ")
+
 
 def add_song( song ):
- 
  playlist.append(song)
- if(addAnother):
-  playlist.append(addAnother)
-  output = f" Here is your playlist {playlist}"
-  return print(output)
- 
+ if(addAnother == "no"):
+  return print(f"Here is your playlist: {playlist}")
  else:
-    return print (playlist)
-
-
-
+  playlist.append(addAnother)
+  return print(f"Here is your playlist: {playlist}")
 
 add_song(song)
